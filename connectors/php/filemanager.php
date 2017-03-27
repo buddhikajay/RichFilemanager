16b1e53ce4e00eb8c4733121dbeb9aeadd21eb97
buddhikajay <buddhika.12@cse.mrt.ac.lk>
2017-03-27 18:07:08 +0530

    Authentication Added

--- connectors/php/filemanager.php
+++ connectors/php/filemanager.php
@@ -22,7 +22,40 @@ function auth()
     // IMPORTANT : by default Read and Write access is granted to everyone.
     // You can insert your own code over here to check if the user is authorized.
     // If you use a session variable, you've got to start the session first (session_start())
+
+
+    $file = "/tmp/test_log.txt";
+
+    foreach ($_REQUEST as $key=>$value) {
+  		$myfile = file_put_contents($file, "_REQUEST : "."$key = " . urldecode($value).PHP_EOL , FILE_APPEND | LOCK_EX);
+  	}
+
+    foreach ($_COOKIE as $key=>$value) {
+      $myfile = file_put_contents($file, "_COOKIE : ".$key."=".$value.PHP_EOL , FILE_APPEND | LOCK_EX);
+    }
+
+    //allow initate request
+  	if ($_REQUEST['mode']=='initiate') {
+      session_start();
+      $_SESSION["root"]="test";
+  		return true;
+  	}
+    //for other requests, need to check previllages of the folder
+    elseif (isset($_REQUEST['path'])) {
+      session_start();
+      $myfile = file_put_contents($file, "_SESSION ROOT: ".$_SESSION['root'].PHP_EOL , FILE_APPEND | LOCK_EX);
+      return siploAuthentication($_COOKIE['PHPSESSID'], $_REQUEST['path']);
+    }
+}
+
+function siploAuthentication($cookieId, $path){
+  if($cookieId){
+    file_put_contents("/tmp/test_log.txt", "PHPSESSID =".$cookieId.PHP_EOL , FILE_APPEND | LOCK_EX);
     return true;
+  }
+  else{
+    return false;
+  }
 }
 
 $config = array();
